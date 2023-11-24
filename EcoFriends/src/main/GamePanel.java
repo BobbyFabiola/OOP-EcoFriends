065b61ba9e329de09306d1af27417ea67aa42ac9
elyzuh <katrina.elyza@gmail.com>
2023-11-24 21:56:16 +0800

    koteng commit test

--- EcoFriends/src/main/GamePanel.java
+++ EcoFriends/src/main/GamePanel.java
@@ -11,11 +11,12 @@ public class GamePanel extends JPanel {
 
     final int tileSize = originalTileSize * scale;          //48x48 tile
     final int maxScreenCol = 25;                            //16
-    final int maxScreenRow = 12;                            //12 nyark
+    final int maxScreenRow = 12;                            //12 nyarkisms
     final int screenWidth = tileSize * maxScreenCol;        //768 pixels
     final int screenHeight = tileSize * maxScreenRow;       //576 pixels
 
     public GamePanel () {
+
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
