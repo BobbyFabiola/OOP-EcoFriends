c1a9ab5926863cbe87ed59f424114842d56747a4
Fabiola Villanueva <22103326@usc.edu.ph>
2023-11-24 13:07:07 +0800

    ReadME with YouTube link

--- EcoFriends/src/main/GamePanel.java
+++ EcoFriends/src/main/GamePanel.java
@@ -10,11 +10,10 @@ public class GamePanel extends JPanel {
     final int scale = 3;
 
     final int tileSize = originalTileSize * scale;          //48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    final int maxScreenCol = 25;                            //16
+    final int maxScreenRow = 12;                            //12
     final int screenWidth = tileSize * maxScreenCol;        //768 pixels
     final int screenHeight = tileSize * maxScreenRow;       //576 pixels
-
 
     public GamePanel () {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OOP-EcoFriends
 A small scale ecosystem simulation game.
+
+Link to YouTube references: https://youtube.com/playlist?list=PLbSHC6qkkd62Le3JsbPwmxUtyIJfHRZNW&si=UiFCmwBdakaVaZNr
+1. Game Panel
