0c0bfd4ad392129cdda92456bb78d1199da8be5b
Fabiola Villanueva <22103326@usc.edu.ph>
2023-11-25 22:17:56 +0800

    Making game loop (YT pt2 vid of playlist)
    Key inputs are only of "A" and "D" for left and right. I used sleep method for the time interval and entity is now properly centered no matter the size of the maximum column number.

--- EcoFriends/src/main/GamePanel.java
+++ EcoFriends/src/main/GamePanel.java
@@ -3,22 +3,88 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;          //48x48 tile
-    final int maxScreenCol = 25;                            //16
-    final int maxScreenRow = 12;                            //12
-    final int screenWidth = tileSize * maxScreenCol;        //768 pixels
-    final int screenHeight = tileSize * maxScreenRow;       //576 pixels
+    final int tileSize = originalTileSize * scale;                                                                      //48x48 tile
+    final int maxScreenCol = 25;                                                                                        //16
+    final int maxScreenRow = 12;                                                                                        //12
+    final int screenWidth = tileSize * maxScreenCol;                                                                    //768 pixels
+    final int screenHeight = tileSize * maxScreenRow;                                                                   //576 pixels
+
+    //FPS
+    int FPS = 30;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;                                                                                                  //keeps program running until you stop it
+
+    //set player's default position
+    int playerX = (screenWidth - tileSize) / 2;
+    int playerY = 450;
+    int playerSpeed = 4;
 
     public GamePanel () {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread () {                                                                                    //instantiating the thread
+
+        gameThread = new Thread(this);
+        gameThread.start ();
+    }
+
+    @Override
+    public void run () {                                                                                                //the game loop, core of the game; sleep method
+//        long currentTime = System.nanoTime();                                                                           //checks current system time
+        double drawInterval = (double) 100000000 /FPS;                                                                  //1 sec / 60 FPS; we draw the screen 60x / sec
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread != null) {
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime /1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }                                                                                                                   //for everytime we run a game thread, it automatically calls the run method
+
+    public void update () {                                                                                             //animal movement
+        if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+
+        playerX = Math.max(0, Math.min(playerX, screenWidth - tileSize));                                               //keeps player within the screen
+        playerY = Math.max(0, Math.min(playerY, screenHeight - tileSize));
+    }
+
+    public void paintComponent (Graphics g) {                                                                           //built in method
+        super.paintComponent(g);
+
+        Graphics g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(playerX, playerY,tileSize, tileSize);                                                               //x, y, width, height
+
+        g2.dispose();
     }
 }
---
+++ EcoFriends/src/main/KeyHandler.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.security.Key;
+
+public class KeyHandler implements KeyListener {
+
+    public boolean leftPressed, rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_A) {                                                                                    //move left
+            leftPressed = true;
+        }
+
+        if (code == KeyEvent.VK_D) {                                                                                    //move right
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_A) {                                                                                    //move left
+            leftPressed = false;
+        }
+
+        if (code == KeyEvent.VK_D) {                                                                                    //move right
+            rightPressed = false;
+        }
+    }
+}
