c084ad493357e653f48cd7affde3709448d1ab26
decaff1115 <147979084+decaff1115@users.noreply.github.com>
2023-12-15 00:56:30 +0800

    Edited the sidequest button in TitleSCreen, edited the font and placements of texts in snake game

--- EcoFriends/src/main/GamePanelMini.java
+++ EcoFriends/src/main/GamePanelMini.java
@@ -6,6 +6,8 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 import java.util.Random;
 
 public class GamePanelMini extends JPanel implements ActionListener{
@@ -19,8 +21,8 @@ public class GamePanelMini extends JPanel implements ActionListener{
     static final int UNIT_SIZE = 50;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
     static final int DELAY = 175;
-    final int x[] = new int[GAME_UNITS];
-    final int y[] = new int[GAME_UNITS];
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
     int bodyParts = 1;
     int applesEaten;
     int appleX;
@@ -29,46 +31,66 @@ public class GamePanelMini extends JPanel implements ActionListener{
     boolean running = false;
     Timer timer;
     Random random;
+    Font minecraft;
+
+
+
 
     GamePanelMini(){
+
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
 
         try {
-            backgroundImage = ImageIO.read(getClass().getResourceAsStream("/player/images/town.png"));      //background setting
+            backgroundImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/images/town.png")));      //background setting
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
-            front = ImageIO.read(getClass().getResourceAsStream("/player/images/trash.png"));      //background setting
+            front = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/images/trash.png")));      //background setting
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
-            player = ImageIO.read(getClass().getResourceAsStream("/player/images/bara_front.png"));      //background setting
+            player = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/images/bara_front.png")));      //background setting
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
-            babeh = ImageIO.read(getClass().getResourceAsStream("/player/images/bara_left.png"));      //background setting
+            babeh = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/images/bara_left.png")));      //background setting
         } catch (IOException e) {
             e.printStackTrace();
         }
+        try {
+            InputStream is = getClass().getResourceAsStream("/player/images/Minecraftia-Regular.ttf");
+            assert is != null;
+            minecraft = Font.createFont(Font.TRUETYPE_FONT, is);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(minecraft);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
+
+
     public void startGame() {
         newApple();
         running = true;
         timer = new Timer(DELAY,this);
         timer.start();
     }
+
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
@@ -103,26 +125,41 @@ public class GamePanelMini extends JPanel implements ActionListener{
                 }
             }
             g.setColor(Color.black);
-            g.setFont( new Font("Helvetica",Font.BOLD, 40));
-            FontMetrics metrics = getFontMetrics(g.getFont());
-            g.drawString("Trash Collected: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+            g.setFont(new Font("Minecraftia", Font.BOLD, 30)); // Use "Minecraftia" as the font name
+
+
+            String trashCollectedText = "Trash Collected: " + applesEaten;
+            g.drawString(trashCollectedText, 920, 60);
+
         }
         else {
             gameOver(g);
         }
 
     }
-    public void newApple(){
-        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+
+public void newApple() {
+    while (true) {
+        appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+
+        boolean onSnake = false;
+        for (int i = 0; i < bodyParts; i++) {
+            if (x[i] == appleX && y[i] == appleY) {
+                onSnake = true;
+                break;
+            }
+        }
+        if (!onSnake) break;
     }
+}
     public void move(){
-        for(int i = bodyParts;i>0;i--) {
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
 
-        switch(direction) {
+        switch (direction) {
             case 'U':
                 y[0] = y[0] - UNIT_SIZE;
                 break;
@@ -139,7 +176,7 @@ public class GamePanelMini extends JPanel implements ActionListener{
 
     }
     public void checkApple() {
-        if((x[0] == appleX) && (y[0] == appleY)) {
+        if ((x[0] == appleX) && (y[0] == appleY)) {
             bodyParts++;
             applesEaten++;
             newApple();
@@ -147,54 +184,66 @@ public class GamePanelMini extends JPanel implements ActionListener{
     }
     public void checkCollisions() {
         //checks if head collides with body
-        for(int i = bodyParts;i>0;i--) {
-            if((x[0] == x[i])&& (y[0] == y[i])) {
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
                 running = false;
+                break;
             }
         }
-        //check if head touches left border
-        if(x[0] < 0) {
+
+        // Check if head touches left border
+        if (x[0] < 0) {
             running = false;
         }
-        //check if head touches right border
-        if(x[0] > SCREEN_WIDTH) {
+        // Check if head touches right border
+        if (x[0] >= SCREEN_WIDTH) {
             running = false;
         }
-        //check if head touches top border
-        if(y[0] < 0) {
+        // Check if head touches top border
+        if (y[0] < 0) {
             running = false;
         }
-        //check if head touches bottom border
-        if(y[0] > SCREEN_HEIGHT) {
+        // Check if head touches bottom border
+        if (y[0] >= SCREEN_HEIGHT) {
             running = false;
         }
 
-        if(!running) {
+        if (!running) {
             timer.stop();
         }
     }
     public void gameOver(Graphics g) {
         //Score
         g.setColor(Color.red);
-        g.setFont( new Font("Helvetica",Font.BOLD, 40));
-        FontMetrics metrics1 = getFontMetrics(g.getFont());
-        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+        g.setFont( new Font("Minecraftia",Font.BOLD, 40));
+        String scoreText = "Score: " + applesEaten;
+        FontMetrics scoreMetrics = getFontMetrics(g.getFont());
+        int scoreX = (SCREEN_WIDTH - scoreMetrics.stringWidth(scoreText)) / 2;
+        int scoreY = SCREEN_HEIGHT / 2 - scoreMetrics.getHeight(); // Adjust the vertical position
+        g.drawString(scoreText, scoreX, scoreY);
         //Game Over text
         g.setColor(Color.red);
-        g.setFont( new Font("Helvetica",Font.BOLD, 75));
+        g.setFont( new Font("Minecraftia",Font.BOLD, 75));
         FontMetrics metrics2 = getFontMetrics(g.getFont());
-        g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+        int gameOverX = (SCREEN_WIDTH - metrics2.stringWidth("Game Over")) / 2;
+        int gameOverY = SCREEN_HEIGHT / 2 + metrics2.getHeight(); // Adjust the vertical position
+        g.drawString("Game Over", gameOverX, gameOverY);
+
+
+
+
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        if(running) {
+        if (timer != null && running) {
             move();
             checkApple();
             checkCollisions();
+            repaint();
         }
-        repaint();
     }
+
 
     public class MyKeyAdapter extends KeyAdapter{
         @Override
@@ -221,6 +270,7 @@ public class GamePanelMini extends JPanel implements ActionListener{
                     }
                     break;
             }
+
         }
     }
 }
