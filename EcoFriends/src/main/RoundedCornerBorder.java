c07ebb5f21b871afc9c24bdf84b19107eb6ac58d
decaff1115 <147979084+decaff1115@users.noreply.github.com>
2023-12-18 09:37:19 +0800

    With Reset button in snakeGame.

---
+++ EcoFriends/src/main/RoundedCornerBorder.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.border.Border;
+
+public class RoundedCornerBorder implements Border {
+    private final int radius;
+
+    public RoundedCornerBorder(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius + 1, radius + 1, radius + 2, radius);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return true;
+    }
+}
+
